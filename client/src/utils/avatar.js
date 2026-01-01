90c0283c29d635285c8306f956ad68e3ab738c32
swl <swl1236915@gmail.com>
2026-01-01 16:01:29 +0900

    Fix: Add cache busting to avatar URLs

--- client/src/utils/avatar.js
+++ client/src/utils/avatar.js
@@ -3,9 +3,9 @@ import { API_URL } from '../config';
 export const getAvatarUrl = (user) => {
     if (!user) return `https://api.dicebear.com/9.x/dylan/svg?seed=default`;
 
-    // If user has uploaded avatar, use it
+    // If user has uploaded avatar, use it with cache busting
     if (user.avatarPath) {
-        return `${API_URL}${user.avatarPath}`;
+        return `${API_URL}${user.avatarPath}?t=${Date.now()}`;
     }
 
     // Otherwise use generated avatar
